c664d95f568815b4fb434be44592f83c9929bdad
Youssef Mohamed <youssef.mu.saber@gmail.com>
2023-07-31 19:04:34 +0300

    Created the LoginScreen file

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,25 @@
 import 'package:bloc/bloc.dart';
+import 'package:coders_oasis/screens/login/login_screen.dart';
 import 'package:coders_oasis/screens/onboarding/onboarding.dart';
 import 'package:coders_oasis/shared/bloc_observer.dart';
 import 'package:coders_oasis/shared/cubit/onboarding_cubit/onboarding_cubit.dart';
 import 'package:coders_oasis/shared/cubit/onboarding_cubit/onboarding_states.dart';
+import 'package:coders_oasis/shared/network/local/cahce_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = MyBlocObserver();
-  runApp(const MyApp());
+  await CacheHelper.init();
+  var onBoarding = CacheHelper.getData(key: 'onBoarding') ?? false;
+  runApp(MyApp(onBoarding));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  bool onBoarding;
+
+  MyApp(this.onBoarding, {super.key,});
 
   // This widget is the root of your application.
   @override
@@ -24,11 +31,20 @@ class MyApp extends StatelessWidget {
             builder: (context, state) => MaterialApp(
                   title: 'Flutter Demo',
                   home: Scaffold(
-                      body: SafeArea(child: OnBoardingScreen())),
+                      body: SafeArea(child: openScreen())),
                   debugShowCheckedModeBanner: false,
                   theme: ThemeData(
                     useMaterial3: true,
                   ),
                 )));
+  }
+
+  Widget openScreen() {
+    if (onBoarding) {
+        print(onBoarding);
+        return LoginScreen();
+    } else {
+      return OnBoardingScreen();
+    }
   }
 }
--- lib/screens/onboarding/onboarding.dart
+++ lib/screens/onboarding/onboarding.dart
@@ -1,5 +1,6 @@
 import 'package:coders_oasis/shared/components/constants.dart';
 import 'package:coders_oasis/shared/cubit/onboarding_cubit/onboarding_cubit.dart';
+import 'package:coders_oasis/shared/cubit/onboarding_cubit/onboarding_states.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -7,7 +8,8 @@ import 'package:hexcolor/hexcolor.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 import '../../shared/components/components.dart';
-import '../../shared/cubit/onboarding_cubit/onboarding_states.dart';
+import '../../shared/network/local/cahce_helper.dart';
+import '../login/login_screen.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   final PageController boardController = PageController();
@@ -17,68 +19,88 @@ class OnBoardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var cubit = OnBoardingCubit.get(context);
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
+    return BlocConsumer<OnBoardingCubit, OnBoardingStates>(
+      listener: (context, state) {},
+      builder: (context, state) => Scaffold(
         backgroundColor: Colors.white,
-        elevation: 0,
-        actions: [
-          TextButton(
-            style: ButtonStyle(
-              overlayColor: MaterialStateProperty.all(defaultColorTransparent)
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          actions: [
+            TextButton(
+                style: ButtonStyle(
+                    overlayColor:
+                        MaterialStateProperty.all(defaultColorTransparent)),
+                onPressed: () {
+                  skipOnBoarding(context);
+                },
+                child: Text(
+                  "Skip",
+                  style: GoogleFonts.rubik(
+                      textStyle:
+                          TextStyle(color: HexColor("78746D"), fontSize: 16)),
+                ))
+          ],
+        ),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 150,
             ),
-              onPressed: () {},
-              child: Text(
-                "Skip",
-                style: GoogleFonts.rubik(
-                    textStyle: TextStyle(color: HexColor("78746D"), fontSize: 16)),
-              ))
-        ],
-      ),
-      body: Column(
-        children: [
-                SizedBox(height: 150,),
-                Expanded(
-                  child: PageView.builder(
-                    controller: boardController,
-                    physics: const BouncingScrollPhysics(),
-                    itemBuilder: (context, index) {
-                      cubit.currentIndex = index;
-                      return onBoardingItem(
-                          cubit.onBoardingItems[cubit.currentIndex]);
-                    },
-                    itemCount: cubit.onBoardingItems.length,
-                  ),
-                ),
-                SmoothPageIndicator(
-                  effect: ExpandingDotsEffect(
-                    dotWidth: 10,
-                    dotHeight: 10,
-                    dotColor: HexColor("D5D4D4"),
-                    activeDotColor: HexColor("65AAEA"),
-                  ),
-                  controller: boardController,
-                  count: cubit.onBoardingItems.length,
-                ),
-          SizedBox(height: 160,),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: defaultButton(
-              textColor: "ffffff",
-              buttonWidth: double.infinity,
-              text: "Next",
-              onTap: () {
-                if (cubit.currentIndex < 2) {
-                  boardController.nextPage(
-                    duration: const Duration(milliseconds: 500),
-                    curve: Curves.linearToEaseOut,
-                  );
-                }
-              },
+            Expanded(
+              child: PageView.builder(
+                controller: boardController,
+                physics: const BouncingScrollPhysics(),
+                itemBuilder: (context, index) {
+                  cubit.currentIndex = index;
+                  return onBoardingItem(
+                      cubit.onBoardingItems[cubit.currentIndex]);
+                },
+                itemCount: cubit.onBoardingItems.length,
+              ),
             ),
-          ),
-        ],
+            SmoothPageIndicator(
+              effect: ExpandingDotsEffect(
+                dotWidth: 10,
+                dotHeight: 10,
+                dotColor: HexColor("D5D4D4"),
+                activeDotColor: HexColor("65AAEA"),
+              ),
+              controller: boardController,
+              count: cubit.onBoardingItems.length,
+            ),
+            SizedBox(
+              height: 160,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: defaultButton(
+                textColor: "ffffff",
+                buttonWidth: double.infinity,
+                text: "Next",
+                onTap: () {
+                  if (cubit.currentIndex < 2) {
+                    boardController.nextPage(
+                      duration: const Duration(milliseconds: 500),
+                      curve: Curves.linearToEaseOut,
+                    );
+                  } else {
+                    skipOnBoarding(context);
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  void skipOnBoarding(context) {
+    CacheHelper.saveData(key: 'onBoarding', value: true).then((value) {
+      if (value) {
+        navigateAndFinish(context, LoginScreen());
+      }
+    });
   }
 }
