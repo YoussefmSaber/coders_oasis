ed383fddae9dab96ebe7793385c26d5fc1ed7899
Youssef Mohamed <youssef.mu.saber@gmail.com>
2023-09-19 15:51:55 +0300

    Added the shimmer effect to courseVideos screen

--- lib/screens/app-screens/course-videos-screen/course_videos.dart
+++ lib/screens/app-screens/course-videos-screen/course_videos.dart
@@ -1,7 +1,9 @@
 import 'package:coders_oasis/models/video_model.dart';
 import 'package:coders_oasis/shared/components/components.dart';
 import 'package:coders_oasis/shared/components/constants.dart';
+import 'package:coders_oasis/shared/components/shimmer_components.dart';
 import 'package:coders_oasis/shared/network/remote/supabase_api.dart';
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -27,122 +29,127 @@ class CourseVideosScreen extends StatelessWidget {
       body: FutureBuilder(
           future: getVideos(course.id),
           builder: (_, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const CircularProgressIndicator();
-            } else if (snapshot.hasError) {
-              return Text("Error: ${snapshot.error}");
-            } else {
-              return Stack(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 16.0),
-                          child: SizedBox(
-                            width: 250,
-                            child: Text(
-                              course.name,
-                              style: GoogleFonts.rubik(
-                                textStyle: TextStyle(
-                                  letterSpacing: -0.5,
-                                  color: darkFont,
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.w500,
-                                ),
+            return Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 16.0),
+                        child: SizedBox(
+                          width: 250,
+                          child: Text(
+                            course.name,
+                            style: GoogleFonts.rubik(
+                              textStyle: TextStyle(
+                                letterSpacing: -0.5,
+                                color: darkFont,
+                                fontSize: 24,
+                                fontWeight: FontWeight.w500,
                               ),
-                              maxLines: 2,
-                              overflow: TextOverflow.ellipsis,
-                              textAlign: TextAlign.center,
                             ),
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.center,
                           ),
                         ),
-                        Card(
-                          elevation: 0,
-                          surfaceTintColor: Colors.white,
-                          color: Colors.white,
-                          shape: RoundedRectangleBorder(
-                              side: BorderSide(width: 1.2, color: gray),
-                              borderRadius: BorderRadius.circular(10)),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              ClipRRect(
-                                borderRadius: const BorderRadius.only(
-                                    topLeft: Radius.circular(10),
-                                    topRight: Radius.circular(10)),
-                                child: Image.network(
-                                  course.thumbnailPath,
-                                  width: double.infinity,
+                      ),
+                      Card(
+                        elevation: 0,
+                        surfaceTintColor: Colors.white,
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(
+                            side: BorderSide(width: 1.2, color: gray),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ClipRRect(
+                              borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(10),
+                                  topRight: Radius.circular(10)),
+                              child: Image.network(
+                                course.thumbnailPath,
+                                width: double.infinity,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 16,
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 16.0),
+                              child: Text(
+                                course.name,
+                                style: GoogleFonts.rubik(
+                                    textStyle: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w500,
+                                        letterSpacing: -.5,
+                                        color: darkFont)),
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                                textAlign: TextAlign.start,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 8,
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 16.0),
+                              child: Text(
+                                course.briefDesc,
+                                style: GoogleFonts.rubik(
+                                    textStyle: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w500,
+                                        letterSpacing: -.5,
+                                        color: darkGray)),
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                                textAlign: TextAlign.start,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 16,
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      ConditionalBuilder(
+                          condition: snapshot.connectionState ==
+                              ConnectionState.waiting,
+                          builder: (_) => Expanded(
+                                child: ListView.separated(
+                                  itemBuilder: (_, index) => videoItemShimmer(),
+                                  separatorBuilder: (_, index) =>
+                                      const SizedBox(height: 8),
+                                  itemCount: 5,
                                 ),
                               ),
-                              const SizedBox(
-                                height: 16,
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 16.0),
-                                child: Text(
-                                  course.name,
-                                  style: GoogleFonts.rubik(
-                                      textStyle: TextStyle(
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.w500,
-                                          letterSpacing: -.5,
-                                          color: darkFont)),
-                                  maxLines: 2,
-                                  overflow: TextOverflow.ellipsis,
-                                  textAlign: TextAlign.start,
-                                ),
-                              ),
-                              const SizedBox(
-                                height: 8,
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 16.0),
-                                child: Text(
-                                  course.briefDesc,
-                                  style: GoogleFonts.rubik(
-                                      textStyle: TextStyle(
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.w500,
-                                          letterSpacing: -.5,
-                                          color: darkGray)),
-                                  maxLines: 2,
-                                  overflow: TextOverflow.ellipsis,
-                                  textAlign: TextAlign.start,
-                                ),
-                              ),
-                              const SizedBox(
-                                height: 16,
-                              ),
-                            ],
-                          ),
-                        ),
-                        SizedBox(
-                          height: 8,
-                        ),
-                        Expanded(
-                          child: ListView.separated(
-                              itemBuilder: (_, index) => videoItem(
-                                  video: snapshot.data![index],
-                                  context: context),
-                              separatorBuilder: (_, index) =>
-                                  const SizedBox(height: 8),
-                              itemCount: snapshot.data!.length),
-                        ),
-                      ],
-                    ),
+                          fallback: (_) => Expanded(
+                                child: ListView.separated(
+                                    itemBuilder: (_, index) => videoItem(
+                                        video: snapshot.data![index],
+                                        context: context),
+                                    separatorBuilder: (_, index) =>
+                                        const SizedBox(height: 8),
+                                    itemCount: snapshot.data!.length),
+                              )),
+                    ],
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: backButton(onTap: () => Navigator.pop(context)),
-                  )
-                ],
-              );
-            }
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: backButton(onTap: () => Navigator.pop(context)),
+                )
+              ],
+            );
           }),
     ));
   }
